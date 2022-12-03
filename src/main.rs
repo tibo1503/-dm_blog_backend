70e709666b20999e91c63848ebea33b52bc7760c
Dofe <thibo1503@gmail.com>
2022-12-03 15:01:24 +0100

    ugly start implemention of articles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rocket::serde::{Serialize, Deserialize, json::Json};
+use rocket::http::Status;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -7,13 +10,54 @@ fn index() -> &'static str {
 
 // Articles
 #[get("/")]
-fn article() -> String {
-    format!("List of articles")
+fn get_articles() -> Result<Json<Vec<Article>>, Status> {
+    let articles = vec![
+        Article {
+            id: 1,
+
+            author_id: 1,
+            author_pseudo: "Dofe".to_string(),
+        
+            title: "Why use Rust ?".to_string(),
+            content: "Security".to_string()
+        },
+        Article {
+            id: 2,
+
+            author_id: 1,
+            author_pseudo: "Dofe".to_string(),
+        
+            title: "What is Wasm ?".to_string(),
+            content: "Any \"alternative\" of JS".to_string()
+        }
+    ];
+
+    Result::Ok(Json(articles))
 }
 
 #[get("/<id>")]
-fn article_id(id: u64) -> String {
-    format!("Article id: {}", id)
+fn get_article(id: u64) -> Result<Json<Article>, Status> {
+    match id {
+        1 => Result::Ok(Json(Article {
+            id: 1,
+
+            author_id: 1,
+            author_pseudo: "Dofe".to_string(),
+        
+            title: "Why use Rust ?".to_string(),
+            content: "Security".to_string()
+        })),
+        2 => Result::Ok(Json(Article {
+            id: 2,
+
+            author_id: 1,
+            author_pseudo: "Dofe".to_string(),
+        
+            title: "What is Wasm ?".to_string(),
+            content: "Any \"alternative\" of JS".to_string()
+        })),
+        id => Result::Err(Status::NotFound)
+    }
 }
 
 #[launch]
@@ -23,5 +67,52 @@ fn rocket() -> _ {
     rocket::build()
         .mount(format!("{}{}", api_url, "/"), routes![index])
         .mount(format!("{}{}", api_url, "/auth"), routes![index])
-        .mount(format!("{}{}", api_url, "/article"), routes![article, article_id])
+        .mount(format!("{}{}", api_url, "/article"), routes![get_articles, get_article])
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Comment {
+    id: i64,
+
+    article_id: i64,
+
+    author_id: i64,
+    author_pseudo: String,
+
+    content: String
+}
+
+struct Tag {
+    id: i64,
+
+    name: String,
+    description: String
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Article {
+    id: i64,
+
+    author_id: i64,
+    author_pseudo: String,
+
+    title: String,
+    content: String
+}
+
+struct User {
+    id: i64,
+    pseudo: String,
+    about: String,
+
+    inscription_date: String,
+    last_connection_date: String
+}
+
+struct Role {
+    id: i64,
+    name: String,
+    description: String
 }
