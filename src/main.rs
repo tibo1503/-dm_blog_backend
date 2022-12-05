3622915ac37fa267f1b3b8f9d16707012af731e8
Dofe <thibo1503@gmail.com>
2022-12-05 11:21:39 +0100

    main.rs file reorganized

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,51 @@ use rocket::http::Status;
 
 #[macro_use] extern crate rocket;
 
+#[launch]
+fn rocket() -> _ {
+    let api_url = "/api/v1_dev".to_string();
+
+    rocket::build()
+        //.mount(format!("{}{}", api_url, "/"), routes![])
+        //.mount(format!("{}{}", api_url, "/auth"), routes![])
+        .mount(format!("{}{}", api_url, "/article_tags"), routes![get_article_tags, get_article_tag])
+        .mount(format!("{}{}", api_url, "/article"), routes![get_articles, get_article])
+}
+
+// Role
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Role {
+    id: i64,
+    name: String,
+    description: String
+}
+
 // Auth
 
 
 // Users
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct User {
+    id: i64,
+    pseudo: String,
+    about: String,
 
+    inscription_date: String,
+    last_connection_date: String
+}
 
 // Article tags
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Tag {
+    id: i64,
+
+    name: String,
+    description: String
+}
+
 #[get("/")]
 fn get_article_tags() -> Result<Json<Vec<Tag>>, Status> {
     let tags = vec![
@@ -46,6 +84,18 @@ fn get_article_tag(id: u64) -> Result<Json<Tag>, Status> {
 }
 
 // Articles
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Article {
+    id: i64,
+
+    author_id: i64,
+    author_pseudo: String,
+
+    title: String,
+    content: String
+}
+
 #[get("/")]
 fn get_articles() -> Result<Json<Vec<Article>>, Status> {
     let articles = vec![
@@ -97,57 +147,7 @@ fn get_article(id: u64) -> Result<Json<Article>, Status> {
     }
 }
 
-#[launch]
-fn rocket() -> _ {
-    let api_url = "/api/v1_dev".to_string();
-
-    rocket::build()
-        //.mount(format!("{}{}", api_url, "/"), routes![])
-        //.mount(format!("{}{}", api_url, "/auth"), routes![])
-        .mount(format!("{}{}", api_url, "/article_tags"), routes![get_article_tags, get_article_tag])
-        .mount(format!("{}{}", api_url, "/article"), routes![get_articles, get_article])
-}
-
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct Role {
-    id: i64,
-    name: String,
-    description: String
-}
-
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct User {
-    id: i64,
-    pseudo: String,
-    about: String,
-
-    inscription_date: String,
-    last_connection_date: String
-}
-
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct Tag {
-    id: i64,
-
-    name: String,
-    description: String
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Article {
-    id: i64,
-
-    author_id: i64,
-    author_pseudo: String,
-
-    title: String,
-    content: String
-}
-
+// Comment
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Comment {
@@ -160,3 +160,4 @@ struct Comment {
 
     content: String
 }
+
